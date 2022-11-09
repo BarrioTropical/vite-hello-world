a96a59fee22789518def6b2483114ac5a9a1cc39
BarrioTropical <barriotropical@gmail.com>
2022-11-09 14:23:01 +0100

    creazione AppTitle e stampa in App.vue l'h1
    Signed-off-by: BarrioTropical <“barriotropical@gmail.com”>

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import AppTitleVue from './components/AppTitle.vue';
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <AppTitleVue msg= "La mia prima app con Vite!" />
     </div>
   </header>
 
   <main>
-    <TheWelcome />
+    <AppTitleVue />
   </main>
 </template>
 
@@ -22,10 +19,7 @@ header {
   line-height: 1.5;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+
 
 @media (min-width: 1024px) {
   header {
@@ -40,8 +34,9 @@ header {
 
   header .wrapper {
     display: flex;
-    place-items: flex-start;
+    place-items: center;
     flex-wrap: wrap;
+    color: white;
   }
 }
 </style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,20 @@
+<script setup>
+    defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+    <h1>
+        {{ msg }}
+    </h1>
+</template>
+
+<style scoped>
+    h1{
+        font-size: 18px;
+    }
+</style>
